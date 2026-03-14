49402408b6bece431f85033ce3989085783e4ff5
49768250+voidwzrd@users.noreply.github.com <addisonmays@mac.com>
2026-03-14 06:30:46 -0400

    updated swift files

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "rcount",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "rcount"
-        ),
+            name: "rcount",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
     ]
 )
